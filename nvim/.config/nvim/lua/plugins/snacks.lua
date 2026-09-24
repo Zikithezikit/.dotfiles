57bee180534bdf6f30008bd3fc4883dadb95434d
Yoav Haimov <haimovyoav.work@gmail.com>
2026-09-24 20:12:23 +0300

    dot files now appear in the pickers

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- Apply to all pickers
+        sources = {
+          files = {
+            hidden = true, -- Show hidden/dotfiles in fuzzy finder
+            -- ignored = false, -- Respect .gitignore
+          },
+          explorer = {
+            hidden = true, -- Show hidden files in file explorer
+          },
+          grep = {
+            hidden = true, -- Search hidden files
+          },
+        },
+      },
+    },
+  },
+}
